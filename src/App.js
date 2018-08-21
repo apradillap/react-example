4424107de189c748e947e6390a08fae3a80bc11d
Adrian Pradilla Pórtoles <apradilla@opensistemas.com>
2018-08-21 12:44:50 +0200

    Create nav and add Bootstrap

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bienvenido a React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav className="navbar navbar-dark bg-dark">
+          <a href="" className="text-white">
+            Tasks
+          </a>
+        </nav>
+        <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
   }
