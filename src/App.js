711fd4daea441a90c59881429e8aff8716ec2617
Adrian Pradilla Pórtoles <apradilla@opensistemas.com>
2018-08-21 12:52:21 +0200

    Create first props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navigation/>
+        <Navigation title="Mi primera navegación"/>
         <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
